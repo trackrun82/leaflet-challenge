10cd5af5758ec913c6ec8e158d4d76ded7cd41f0
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-09-10 17:35:53 -0600

    updated circle marker sizes

--- Leaflet-Step-1/static/js/level1.js
+++ Leaflet-Step-1/static/js/level1.js
@@ -19,13 +19,17 @@ function createMap(earthquakes) {
     });
   }
   
-// Create function to create markers with pop-ups
+// Define a markerSize function that will give each earthquake a different radius based on its magnitude
+function markerSize(magnitude) {
+  return magnitude * 20000;
+}
+
+  // Create function to create markers with pop-ups
 function createMarkers(response) {
        
   // Pull the "earthquake" features off of response.data
   const quakes = response.features;
 
- 
   // Create an array to hold earthquake markers
   // Loop through the earthquake array
   const quakeMarkers = quakes.map( quake => {
@@ -53,8 +57,7 @@ function createMarkers(response) {
       color: color,
       fillColor: color,
       fillOpacity: 0.5,
-      radius: 500
-      // radius: quake.properties.mag
+      radius: markerSize(quake.properties.mag)
     })
       .bindPopup("<h3>" + quake.properties.place + 
       "</h3><hr><p>Time: " + new Date(quake.properties.time) + 
@@ -62,10 +65,43 @@ function createMarkers(response) {
       "</p><hr><p> Significance: " + quake.properties.sig + "</p>");
   });
 
-  // Create a layer group made from the earthquare markers array, pass it into the createMap function
+  // Create a layer group made from the earthquake markers array, pass it into the createMap function
   createMap(L.layerGroup(quakeMarkers));
 }
-  
-  
+
+// Set up the legend
+function getColor(d) {
+  return d > 1000 ? '#4d0000' :
+         d > 750  ? '#b30000' :
+         d > 500  ? '#ff0000' :
+         d > 250  ? '#ff8080' :
+                    '#ffe6e6';
+}
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    significance = ["0-250", "250-500", "500-750", "750-1000", "1000+"],
+    labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < significance.length; i++) {
+
+      div.innerHTML += 
+      labels.push(
+          '<i class="circle" style="background:' + getColor(significance[i]) + '"></i> ' +
+      (significance[i] ? significance[i] : '+'));
+
+  }
+  div.innerHTML = labels.join('<br>');
+
+
+return div;
+console.log(div)
+};
+
+// legend.addTo(map);
+
 // Perform an API call to the Earthquake API to get info. Call createMarkers when complete
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson", createMarkers);
