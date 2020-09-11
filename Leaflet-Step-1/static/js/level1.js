4258396c533a8f6f675aa05bb8e18f9b4eda41ad
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-09-11 15:31:30 -0600

    updated bonus section

--- Leaflet-Step-1/static/js/level1.js
+++ Leaflet-Step-1/static/js/level1.js
@@ -9,6 +9,8 @@ function createMap(earthquakes) {
   // Create the tile layer that will be the background of our map
   const darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    noWrap: true,
+    continuousWorld: true,
     maxZoom: 18,
     id: "dark-v10",
     accessToken: API_KEY
